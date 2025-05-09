748e402dff88d57e814e45c07bd779799bac9c07
nik-zsh <nikhillguptaa@protonmail.com>
2025-05-09 18:48:29 +0530

    Adding completions support while debugging

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -1,56 +1,57 @@
 return {
-    { "hrsh7th/cmp-nvim-lsp" },
-    {
-        "L3MON4D3/LuaSnip",
-        dependencies = {
-            "saadparwaiz1/cmp_luasnip",
-            "rafamadriz/friendly-snippets",
-        },
-    },
-    {
-        "hrsh7th/nvim-cmp",
-        dependencies = { "hrsh7th/cmp-path", "rcarriga/cmp-dap" },
-        config = function()
-            local cmp = require("cmp")
-            require("luasnip.loaders.from_vscode").lazy_load()
+	{ "hrsh7th/cmp-nvim-lsp" },
+	{
+		"L3MON4D3/LuaSnip",
+		dependencies = {
+			"saadparwaiz1/cmp_luasnip",
+			"rafamadriz/friendly-snippets",
+		},
+	},
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = { "hrsh7th/cmp-path", "rcarriga/cmp-dap", "hrsh7th/cmp-nvim-lsp" },
 
-            local function is_dap_available()
-                local dap_ok, dap = pcall(require, "dap")
-                return dap_ok and dap and dap.is_initialized ~= nil and dap.is_initialized()
-            end
+		config = function()
+			enabled = function()
+				return vim.api.nvim_get_option_value("buftype", {}) ~= "prompt" or require("cmp_dap").is_dap_buffer()
+			end
+			local cmp = require("cmp")
+			require("luasnip.loaders.from_vscode").lazy_load()
 
-            cmp.setup({
-                enabled = function()
-                    return vim.api.nvim_buf_get_option(0, "buftype") ~= "prompt" and is_dap_available()
-                end,
-                snippet = {
-                    expand = function(args)
-                        require("luasnip").lsp_expand(args.body)
-                    end,
-                },
-                window = {
-                    completion = cmp.config.window.bordered(),
-                    documentation = cmp.config.window.bordered(),
-                },
-                mapping = cmp.mapping.preset.insert({
-                    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-                    ["<C-f>"] = cmp.mapping.scroll_docs(4),
-                    ["<C-Space>"] = cmp.mapping.complete(),
-                    ["<C-e>"] = cmp.mapping.abort(),
-                    ["<CR>"] = cmp.mapping.confirm({ select = true }),
-                }),
-                sources = cmp.config.sources({
-                    { name = "luasnip" },
-                    { name = "nvim_lsp" },
-                    { name = "path" },
-                    { name = "dap" },
-                }, { { name = "buffer" } }),
-            })
+			local function is_dap_available()
+				local dap_ok, dap = pcall(require, "dap")
+				return dap_ok and dap and dap.is_initialized ~= nil and dap.is_initialized()
+			end
 
-            -- Optional: Specific sources for DAP-related filetypes
-            cmp.setup.filetype({ "dap-repl", "dapui_watches", "dapui_hover" }, {
-                sources = { { name = "dap" } },
-            })
-        end,
-    },
+			cmp.setup({
+				snippet = {
+					expand = function(args)
+						require("luasnip").lsp_expand(args.body)
+					end,
+				},
+				window = {
+					completion = cmp.config.window.bordered(),
+					documentation = cmp.config.window.bordered(),
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<C-b>"] = cmp.mapping.scroll_docs(-4),
+					["<C-f>"] = cmp.mapping.scroll_docs(4),
+					["<C-Space>"] = cmp.mapping.complete(),
+					["<C-e>"] = cmp.mapping.abort(),
+					["<CR>"] = cmp.mapping.confirm({ select = true }),
+				}),
+				sources = cmp.config.sources({
+					{ name = "luasnip" },
+					{ name = "nvim_lsp" },
+					{ name = "path" },
+					{ name = "dap" },
+				}, { { name = "buffer" } }),
+			})
+
+			-- Optional: Specific sources for DAP-related filetypes
+			cmp.setup.filetype({ "dap-repl", "dapui_watches", "dapui_hover" }, {
+				sources = { { name = "dap" } },
+			})
+		end,
+	},
 }
